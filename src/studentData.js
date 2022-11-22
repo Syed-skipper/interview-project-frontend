5346c90151b728a4a0788f64f7457c7ca5fe0217
syed <syedsulthan868@gmail.com>
2022-11-22 22:53:25 +0530

    newcommit

--- src/studentData.js
+++ src/studentData.js
@@ -32,7 +32,7 @@ function StudentData() {
     <>
       <div>
         
-        <h3 style={{ textAlign: "center", fontSize: "2rem" }}>Students Data</h3>
+        <h3 style={{ textAlign: "center", fontSize: "2rem",backgroundColor:'#58D68D' }}>Students Data</h3>
 
         <div style={{ display: "flex", justifyContent: "center" }}>
           <Button variant="contained" onClick={handleNavigate}>
