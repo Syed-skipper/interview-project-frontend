a06a2c0f6078e19ae08654191dab07e42a8c0921
syed <syedsulthan868@gmail.com>
2022-11-23 00:19:42 +0530

    new

--- src/Components/studentData.js
+++ src/Components/studentData.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom/dist";
-import "./App.css";
 import {
   Table,
   TableBody,
